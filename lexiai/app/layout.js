38259f23e8749726eb2918b984df263357a43b38
minahil-105 <jeonminahil105@gmail.com>
2024-08-16 15:59:31 -0700

    feat: light mode , dark mode

--- lexiai/app/layout.js
+++ lexiai/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from './providers';
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,12 +14,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <Providers>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={`${inter.className}`}>
-          <header className="bg-gray-800 text-white">
-            <Navbar />
-          </header>
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <header className="bg-gray-800 text-white">
+              <Navbar />
+            </header>
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </Providers>
