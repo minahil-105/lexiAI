5594be1e9f0a174da4b057aaccf449fe465161ab
minahil-105 <jeonminahil105@gmail.com>
2024-08-17 11:45:13 -0700

    fix: fully Responsive feat: dark/white color compatibility

--- lexiai/app/pricing/page.jsx
+++ lexiai/app/pricing/page.jsx
@@ -12,26 +12,39 @@ import { pricing } from '@/lib/data';
 
 const Page = () => {
   return (
-    <section className='mx-20 my-12 text-center'>
-      <h2 className="text-3xl font-bold mb-8">Pricing</h2>
-      <div className="flex items-start justify-center flex-wrap gap-5 w-full">
+    <section className='mx-4 md:mx-10 lg:mx-20 my-8 md:my-12 text-center'>
+      <h2 className="text-2xl md:text-3xl font-bold mb-8 md:mb-12 text-indigo-900 dark:text-indigo-300">
+        Pricing
+      </h2>
+      <div className="flex flex-col sm:flex-row items-start justify-center flex-wrap gap-4 sm:gap-5 w-full">
         {
           pricing.map(plan => (
-            <Card className="w-1/4 shadow-md" key={plan.title}>
+            <Card 
+              key={plan.title}
+              className="w-full max-w-xs p-4 bg-gradient-to-r from-white to-indigo-50 dark:from-gray-800 dark:to-gray-900 rounded-lg shadow-md dark:shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl dark:hover:shadow-2xl hover:bg-indigo-100 dark:hover:bg-gray-700"
+            >
               <CardHeader>
-                <CardTitle>{plan.title}</CardTitle>
-                <CardDescription>{plan.price}</CardDescription>
+                <CardTitle className="text-lg md:text-xl font-semibold text-purple-700 dark:text-purple-400">
+                  {plan.title}
+                </CardTitle>
+                <CardDescription className="text-indigo-600 dark:text-indigo-300">
+                  {plan.price}
+                </CardDescription>
               </CardHeader>
 
               <CardContent>
-                <ul>
+                <ul className="list-disc list-inside text-left text-indigo-700 dark:text-indigo-300">
                   {plan.features.map((feature, index) => (
-                    <li key={index}>{feature}</li>
+                    <li key={index} className="mb-2">
+                      {feature}
+                    </li>
                   ))}
                 </ul>
               </CardContent>
-              <CardFooter className="flex justify-center">
-                <Button>Get Started</Button>
+              <CardFooter className="flex justify-center mt-4">
+                <Button className="px-4 py-2 bg-gradient-to-r from-purple-500 to-indigo-500 text-white rounded-full shadow-lg hover:from-purple-600 hover:to-indigo-600 dark:shadow-purple-900 transition duration-300 ease-in-out">
+                  Get Started
+                </Button>
               </CardFooter>
             </Card>
           ))
--- lexiai/components/Navbar.js
+++ lexiai/components/Navbar.js
@@ -1,164 +1,196 @@
 "use client";
-import React, { useState, useRef } from 'react';
-import { Book, Plus } from 'lucide-react';
+import React, { useState, useRef } from "react";
+import { Book, Plus } from "lucide-react";
 import { useUser, UserButton } from "@clerk/nextjs";
-import Link from 'next/link';
-import { Button } from './ui/button';
-import { navLinks } from '@/lib/data';
-import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { navLinks } from "@/lib/data";
+import { usePathname } from "next/navigation";
 import { Textarea } from "@/components/ui/textarea";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-    DialogClose
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
 } from "@/components/ui/dialog";
-import { Input } from './ui/input';
-import { createDeck } from '@/app/actions/flashcards';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { ModeToggle } from "@/components/mode-toggle"; 
+import { Input } from "./ui/input";
+import { createDeck } from "@/app/actions/flashcards";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { ModeToggle } from "@/components/mode-toggle";
 import { useTheme } from "next-themes";
 
 const Navbar = () => {
-    const { isSignedIn } = useUser();
-    const pathname = usePathname();
-    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-    const { theme, setTheme } = useTheme();
-    // Create form dialog fields
-    const nameRef = useRef(null);
-    const descriptionRef = useRef(null);
-    const contentRef = useRef(null);
+  const { isSignedIn } = useUser();
+  const pathname = usePathname();
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { theme, setTheme } = useTheme();
+  // Create form dialog fields
+  const nameRef = useRef(null);
+  const descriptionRef = useRef(null);
+  const contentRef = useRef(null);
 
-    const queryClient = useQueryClient();
-    const { mutate: handleCreateDeck } = useMutation({
-        mutationFn: async (formData) => {
-            await createDeck(formData);
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries('decks');
-            console.log('Deck created successfully');
-        }
-    });
+  const queryClient = useQueryClient();
+  const { mutate: handleCreateDeck } = useMutation({
+    mutationFn: async (formData) => {
+      await createDeck(formData);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries("decks");
+      console.log("Deck created successfully");
+    },
+  });
 
-    return (
-        <nav className={`flex items-center justify-between p-4 shadow-md lg:px-32 md:px-12 sm:px-6 ${
-            theme === "light"
-              ? "bg-gradient-to-r from-pink-100 to-blue-400 to-purple-600 text-gray-800"
-              : "bg-gradient-to-r from-gray-900 to-indigo-800 to-purple-900 text-white"
-          } relative`}>
-            <Link href="/" className="flex items-center space-x-4">
-                <Book className={`h-8 w-8 ${theme === "light" ? "text-gray-800" : "text-white"}`} />
-                <span className="text-xl font-bold">Lexi AI</span>
-            </Link>
+  return (
+    <nav
+      className={`flex items-center justify-between p-4 shadow-md lg:px-32 md:px-12 sm:px-6 ${
+        theme === "light"
+          ? "bg-gradient-to-r from-pink-200 to-blue-400 to-purple-600 text-gray-800"
+          : "bg-gradient-to-r from-gray-900 to-indigo-800 to-purple-900 text-white"
+      } relative`}
+    >
+      <Link href="/" className="flex items-center space-x-4">
+        <Book
+          className={`h-8 w-8 ${
+            theme === "light" ? "text-gray-800" : "text-white"
+          }`}
+        />
+        <span className="text-xl font-bold">Lexi AI</span>
+      </Link>
 
-            <div className="hidden lg:flex gap-2 md:gap-4">
-                {navLinks.map((link, index) => (
-                    <Link href={link.href} key={index}>
-                        <Button
-                            variant="ghost"
-                            className="rounded-2xl py-0 hover:bg-purple-200 hover:text-gray-800 transition-colors"
-                        >
-                            {link.title}
-                        </Button>
-                    </Link>
-                ))}
-            </div>
+      <div className="hidden lg:flex gap-2 md:gap-4">
+        {navLinks.map((link, index) => (
+          <Link href={link.href} key={index}>
+            <Button
+              variant="ghost"
+              className="rounded-2xl py-0 hover:bg-purple-200 hover:text-gray-800 transition-colors"
+            >
+              {link.title}
+            </Button>
+          </Link>
+        ))}
+      </div>
 
-            <div className="flex items-center gap-3">
-            <ModeToggle />
-                <Dialog>
-                    <DialogTrigger asChild>
-                        <Button
-                            variant="ghost"
-                            size="icon"
-                            className="rounded-full group hover:bg-white transition-colors"
-                        >
-                            <Plus
-                                size={18}
-                                className="text-white group-hover:text-black transition-colors"
-                            />
-                        </Button>
-                    </DialogTrigger>
-                    <DialogContent>
-                        <DialogHeader>
-                            <DialogTitle>Create new Deck</DialogTitle>
-                            <DialogDescription>
-                                Create a new deck to store your flashcards and let AI do the rest.
-                            </DialogDescription>
-                        </DialogHeader>
-                        <div className='flex flex-col gap-2'>
-                            <Input placeholder="Deck Name" ref={nameRef} />
-                            <Input placeholder="Deck Description" ref={descriptionRef} />
-                            <Textarea placeholder="Generate using Lexi AI (leave blank for empty deck)" ref={contentRef} />
-                            <DialogClose asChild>
-                                <Button onClick={() => handleCreateDeck({
-                                    name: nameRef.current.value,
-                                    description: descriptionRef.current.value,
-                                    content: contentRef.current.value
-                                })} className="w-full">Create Deck</Button>
-                            </DialogClose>
-                        </div>
-                    </DialogContent>
-                </Dialog>
-
-                {!isSignedIn ? (
-                    <>
-                        <Link href="/sign-in">
-                            <Button
-                                variant="outline"
-                                className="bg-white text-black border-white transition-colors"
-                            >
-                                Login
-                            </Button>
-                        </Link>
-                    </>
-                ) : (
-                    <UserButton />
-                )}
-
-                {/* Mobile menu button */}
-                <button
-                    onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
-                    className="lg:hidden text-white"
+      <div className="flex items-center gap-3">
+        <ModeToggle />
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="relative rounded-full group hover:bg-white transition-colors"
+            >
+              <Plus
+                size={18}
+                className="text-white group-hover:text-black transition-colors"
+                title="Add deck"
+              />
+              <span className="absolute bottom-[-30px] left-1/2 transform -translate-x-1/2 px-2 py-1 text-xs text-white bg-black rounded opacity-0 group-hover:opacity-100 dark:bg-white dark:text-black">
+                Add deck
+              </span>
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create new Deck</DialogTitle>
+              <DialogDescription>
+                Create a new deck to store your flashcards and let AI do the
+                rest.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex flex-col gap-2">
+              <Input placeholder="Deck Name" ref={nameRef} />
+              <Input placeholder="Deck Description" ref={descriptionRef} />
+              <Textarea
+                placeholder="Generate using Lexi AI (leave blank for empty deck)"
+                ref={contentRef}
+              />
+              <DialogClose asChild>
+                <Button
+                  onClick={() =>
+                    handleCreateDeck({
+                      name: nameRef.current.value,
+                      description: descriptionRef.current.value,
+                      content: contentRef.current.value,
+                    })
+                  }
+                  className="w-full"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-                    </svg>
-                </button>
+                  Create Deck
+                </Button>
+              </DialogClose>
             </div>
+          </DialogContent>
+        </Dialog>
 
-            {/* Mobile Menu */}
-            {isMobileMenuOpen && (
-                <div className="lg:hidden absolute top-16 right-0 w-48 bg-gradient-to-r from-teal-400 to-purple-500 text-white shadow-lg p-4 rounded-lg z-10 overflow-y-auto">
-                    {navLinks.map((link, index) => (
-                        <Link href={link.href} key={index}>
-                            <Button
-                                variant="ghost"
-                                className="w-full text-left hover:bg-white hover:text-black transition-colors mb-2"
-                            >
-                                {link.title}
-                            </Button>
-                        </Link>
-                    ))}
-                    {!isSignedIn ? (
-                        <Link href="/sign-in">
-                            <Button
-                                variant="outline"
-                                className="w-full bg-white text-black border-white transition-colors"
-                            >
-                                Login
-                            </Button>
-                        </Link>
-                    ) : (
-                        <UserButton />
-                    )}
-                </div>
-            )}
-        </nav>
-    );
+        {!isSignedIn ? (
+          <>
+            <Link href="/sign-in">
+              <Button
+                variant="outline"
+                className="bg-white text-black border-white transition-colors"
+              >
+                Login
+              </Button>
+            </Link>
+          </>
+        ) : (
+          <UserButton />
+        )}
+
+        {/* Mobile menu button */}
+        <button
+          onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+          className="lg:hidden text-white"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      {isMobileMenuOpen && (
+        <div className="lg:hidden absolute top-16 right-0 w-48 bg-gradient-to-r from-teal-400 to-purple-500 text-white shadow-lg p-4 rounded-lg z-10 overflow-y-auto">
+          {navLinks.map((link, index) => (
+            <Link href={link.href} key={index}>
+              <Button
+                variant="ghost"
+                className="w-full text-left hover:bg-white hover:text-black transition-colors mb-2"
+              >
+                {link.title}
+              </Button>
+            </Link>
+          ))}
+          {!isSignedIn ? (
+            <Link href="/sign-in">
+              <Button
+                variant="outline"
+                className="w-full bg-white text-black border-white transition-colors"
+              >
+                Login
+              </Button>
+            </Link>
+          ) : (
+            <UserButton />
+          )}
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
